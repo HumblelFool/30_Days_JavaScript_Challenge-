725d465df63158c773bc21e4cfa9f6e8ebbc437e
HumblelFool <Bhairitesh17@gmail.com>
2024-07-13 09:05:40 +0530

    Day_1

---
+++ Day1.js
@@ -0,0 +1,21 @@
+// Activity 1
+var number = 1;
+let string = "sudhanshu shekhar";
+
+console.log("number :", number);
+console.log("name", string);
+
+// Activity 2
+
+const boolean = false;
+console.log(boolean);
+
+// Activity 3
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+
+// Activity 4
+number = 2;
+console.log(number);
